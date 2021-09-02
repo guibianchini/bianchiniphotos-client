4d76f531a707b455232e5ad1c476da495afc5ad9
Guilherme Bianchini <bianchiniguilherme0@gmail.com>
2021-09-01 23:05:24 -0300

    removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './js/App';
-import reportWebVitals from './js/reportWebVitals';
 import { BrowserRouter as Router} from 'react-router-dom';
 
 
@@ -12,8 +11,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
