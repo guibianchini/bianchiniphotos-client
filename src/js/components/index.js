fefd965d9be111672a0524b9233f77edcf3e54be
Guilherme Bianchini <bianchiniguilherme0@gmail.com>
2021-09-05 23:06:06 -0300

    carousel implemented

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,2 +1,3 @@
 export { default as Footer } from './footer.js';
+export { default as ControlledCarousel } from './carousel.js';
 export { default as Menu } from './menu.js';
--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,5 +1,5 @@
 import "../../css/home.css";
-import { Menu, Footer } from "../components";
+import { Menu, Footer, ControlledCarousel } from "../components";
 
 const Home = () => {
   return (
@@ -8,11 +8,16 @@ const Home = () => {
       <div className="content">
         <p>Oi, me chamo Gui!</p>
         <p className="content-sub">
+          <ControlledCarousel />
           <br />
-          Ajudo VOCÊ a se enxergar por outro olhar!<br /><br />
-          Sou fotógrafo há mais de 2 anos.<br /><br /> 
-          Além de ser apaixonado por fotografia, também amo editar as fotos
-          nos mínimos detalhes. <br /> <br />
+          Ajudo VOCÊ a se enxergar por outro olhar!
+          <br />
+          <br />
+          Sou fotógrafo há mais de 2 anos.
+          <br />
+          <br />
+          Além de ser apaixonado por fotografia, também amo editar as fotos nos
+          mínimos detalhes. <br /> <br />
           {/* Sou fotógrafo há mais de 2 anos.<br /><br />
           Sou apaixonado por fotografia e amo também editar os mínimos detalhes.<br /><br />
           Bom, agora deixa eu te mostrar meus pacotes pra você escolher o que mais te agrada.       */}
